b02a989846ac544b65944e92e0b6be21747094fa
Matthew Farrell <farrellm@alum.mit.edu>
2020-08-26 16:50:33 -0400

    add fst and snd

--- src/Zaphod/Base.hs
+++ src/Zaphod/Base.hs
@@ -31,25 +31,26 @@ zTuple2 x y = ZPair x $ zTuple1 y
 zTuple3 :: ZType -> ZType -> ZType -> ZType
 zTuple3 x y z = ZPair x $ zTuple2 y z
 
--- [ CVariable
---     (Variable "car")
---     (ZForall a . ZForall b $ ZFunction zab za),
---   CVariable
---     (Variable "cdr")
---     (ZForall a . ZForall b $ ZFunction zab zb)
--- ]
-
 baseEnvironment :: Environment
 baseEnvironment =
   M.fromList
-    [ ("Top", EType ZTop),
+    [ -- Native values
+      ("Top", EType ZTop),
+      -- Native functions
       ("cons", ENative2 (Native2 $ \l r -> EPair l r (ZPair (exprType l) (exprType r))) zCons),
+      ("fst", ENative1 (Native1 $ fst . getPair) zFst),
+      ("snd", ENative1 (Native1 $ snd . getPair) zSnd),
+      --
       ("zcons", ENative2 (Native2 $ \l r -> EType (ZPair (getType l) (getType r))) zZCons),
+      -- Special forms
       ("if-nil", ESpecial zIfNil),
+      -- bypass type checker
       ("unsafe-coerce", ELambda' [Variable "x"] (ESymbol "x" zb) mempty zUnsafeCoerce)
     ]
   where
     zCons = ZForall a . ZForall b $ ZFunction (zTuple2 za zb) (ZPair za zb)
+    zFst = ZForall a . ZForall b $ ZFunction (zTuple1 (ZPair za zb)) za
+    zSnd = ZForall a . ZForall b $ ZFunction (zTuple1 (ZPair za zb)) zb
     zZCons = ZFunction (zTuple2 (ZType 0) (ZType 0)) (ZType 0)
     zIfNil = ZForall a . ZForall b $ ZFunction (zTuple3 za zb zb) zb
     zUnsafeCoerce = ZForall a . ZForall b $ ZFunction (zTuple1 za) zb
@@ -57,3 +58,7 @@ baseEnvironment =
 getType :: Typed -> ZType
 getType (EType z) = z
 getType _ = bug TypeMismatch
+
+getPair :: Typed -> (Typed, Typed)
+getPair (EPair l r _) = (l, r)
+getPair _ = bug TypeMismatch
